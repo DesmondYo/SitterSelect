1ff898b3fc387f01b41847377375e0f09a6811cc
Desmond Young <Desmond.Young25@yahoo.com>
2021-03-25 12:09:47 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import {LoginPage} from './src/pages/login-page';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => LoginPage);
---
+++ src/pages/login-page.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {View} from 'react-native';
+import {styles} from './styles/login-page-style.js';
+
+export function LoginPage() {
+  return (
+    <View style={styles.loginPageContainer}>{/* Your component here */}</View>
+  );
+}
